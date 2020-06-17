9911ed2bf2e4a667d33e05172072813cd8bd4b31
Natalia Quezada <Naty@MacBook-Pro-de-Natalia.local>
2020-06-17 16:51:42 -0400

    Cambio 1  .

--- src/cipher.js
+++ src/cipher.js
@@ -1,5 +1,37 @@
 const cipher = {
-  // ...
+encode: encodeMessage,
+ decode: decodeMessage,
 };
-
-export default cipher;
+cipher.exports = require("../cipher.spec.js")
+function encodeMessage(offset, string) {
+  let wordEncrypted = "";
+  let uppercaseMessage = string.toUpperCase();
+  offset = Number(offset);
+  for (let index = 0; index < uppercaseMessage.length; index++) {
+    let x = uppercaseMessage.charCodeAt(index);
+    let positionAscci = (x - 65 + offset) % 26 + 65;
+    if (x < 65 || x > 90) {
+      wordEncrypted = wordEncrypted + uppercaseMessage[index];
+      continue;
+    }
+    let encryptedLetter = String.fromCharCode(positionAscci);
+    wordEncrypted = wordEncrypted + encryptedLetter;
+  }
+  return wordEncrypted;
+}
+function decodeMessage(offset, string) {
+  let wordDecrypted = "";
+  let uppercaseMessage = string.toUpperCase();
+  offset = Number(offset);
+  for (let index = 0; index < uppercaseMessage.length; index++) {
+    let x = uppercaseMessage.charCodeAt(index);
+    let positionAscci = (x + 65 - offset) % 26 + 65;
+    if (x < 65 || x > 90) {
+      wordDecrypted = wordDecrypted + uppercaseMessage[index];
+      continue;
+    }
+    let letraEncriptada = String.fromCharCode(positionAscci);
+    wordDecrypted = wordDecrypted + letraEncriptada;
+  }
+  return wordDecrypted;
+}
--- test/cipher.spec.js
+++ test/cipher.spec.js
@@ -1,34 +1,24 @@
 // Importamos el objeto `cipher`, que contiene los métodos `encode` y `decode`
-import cipher from '../src/cipher';
-
+//import cipher from '../src/cipher';
+import cipher from "../src/cipher.js";
 describe('cipher', () => {
-
-  test('should be an object', () => {
-    expect(typeof cipher).toBe('object');
-  });
-
-  describe('cipher.encode', () => {
-
-    test('should be a function', () => {
-      expect(typeof cipher.encode).toBe('function');
+    test('should be an object', () => {
+        expect(typeof cipher).toBe('object');
     });
-    test('should return "HIJKLMNOPQRSTUVWXYZABCDEFG" for "ABCDEFGHIJKLMNOPQRSTUVWXYZ" with offset 33', () => {
-      //completa este test!
+    describe('cipher.encode', () => {
+        test('should be a function', () => {
+            expect(typeof cipher.encode).toBe('function');
+        });
+        test('should return "HIJKLMNOPQRSTUVWXYZABCDEFG" for "ABCDEFGHIJKLMNOPQRSTUVWXYZ" with offset 33', () => {
+            expect(cipher.encode(33, "ABCDEFGHIJKLMNOPQRSTUVWXYZ")).toBe("HIJKLMNOPQRSTUVWXYZABCDEFG");
+        });
     });
-
-    
-  });
-
-  describe('cipher.decode', () => {
-
-    test('should be a function', () => {
-      expect(typeof cipher.decode).toBe('function');
+    describe('cipher.decode', () => {
+        test('should be a function', () => {
+            expect(typeof cipher.decode).toBe('function');
+        });
+        test('should return "ABCDEFGHIJKLMNOPQRSTUVWXYZ" for "HIJKLMNOPQRSTUVWXYZABCDEFG" with offset 33', () => {
+            expect(cipher.decode(33, "HIJKLMNOPQRSTUVWXYZABCDEFG")).toBe("ABCDEFGHIJKLMNOPQRSTUVWXYZ");
+        });
     });
-
-    test('should return "ABCDEFGHIJKLMNOPQRSTUVWXYZ" for "HIJKLMNOPQRSTUVWXYZABCDEFG" with offset 33', () => {
-      //completa este test!
-    });
-
-  });
-
 });
